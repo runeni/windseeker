a61acb2730099b99539d59a483da661baa4503be
Rune Mydske Nielsen <rune.nielsen@knowit.no>
2018-03-28 12:24:24 +0200

    Use postgres

--- windseekerapp/Startup.cs
+++ windseekerapp/Startup.cs
@@ -26,8 +26,8 @@ namespace windseekerapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddEntityFrameworkNpgsql().AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
